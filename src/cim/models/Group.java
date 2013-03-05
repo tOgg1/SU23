a7dcb3d7cf44a40dacc7c5f4c941c83e0967e100
Emil Gronnbeck <emilgroenn@gmail.com>
2013-03-05 15:11:38 +0100

    Laget Group.java

---
+++ src/cim/models/Group.java
@@ -0,0 +1,33 @@
+package cim.models;
+
+import java.util.ArrayList;
+public class Group implements Attendable {
+	String name;
+	Account owner;
+	Calendar calendar;
+	ArrayList<Attendable> members;
+	
+	public void addMember(Attendable member){
+		this.members.add(member);
+	}
+	
+	public void removeMember(Attendable member){
+		//kan ikke slette eieren
+		if (this.owner != member){
+			this.members.remove(member);
+		}
+	}
+	
+	public void receiveInvite(Meeting meeting) {
+	
+	}
+	
+	public void changeOwner(Account owner){
+		this.owner = owner;
+		//Hva skjer med den forrige eieren ? 
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
