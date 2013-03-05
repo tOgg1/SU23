a0dedb94423cd5fd486e8ab8b216c8368ce76ca4
Hakon Amdal <hakon.amdal2@gmail.com>
2013-03-05 14:50:13 +0100

    La til Authenticator og database-interface

---
+++ src/cim/util/AuthenticatorInterface.java
@@ -0,0 +1,26 @@
+package cim.util;
+
+import cim.models.*;
+
+public interface AuthenticatorInterface {
+	/**
+	 * Checks if user is authenticated
+	 * @return true if user is authenticated
+	 */
+	public boolean isAuthenticated();
+	
+	/**
+	 *  Authenticates a user
+	 * @param email User email
+	 * @param password User password, unhashed
+	 * @return true if authenticated, false otherwise
+	 */
+	public boolean authenticate(String email, String password);
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Account getSelf();
+	
+}
