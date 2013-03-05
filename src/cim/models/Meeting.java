17444d3d271d2d4fba08e0824558515f154d65f6
Emil Gronnbeck <emilgroenn@gmail.com>
2013-03-05 14:53:04 +0100

    Fikset Meeting.java

--- src/cim/models/Meeting.java
+++ src/cim/models/Meeting.java
@@ -1,8 +1,9 @@
 package cim.models;
 
+import java.util.Date;
 import java.util.Map;
 
-public class Meeting extends Account
+public class Meeting extends Appointment
 {
 	private String info;
 	private Map<Integer, Attendable> invitees;
@@ -10,13 +11,11 @@ public class Meeting extends Account
 	private Date startDate;
 	private Date endDate;
 	
-	public Meeting(String info, Map<Integer, Attendable> invitees, Room room)
-	{
+	public Meeting(String info, Map<Integer, Attendable> invitees, Room room, Date startDate, Date endDate)
+	{	
+		super(startDate, endDate, info);
 		this.info = info;
-		this.invitees = invitees;
-		this.startDate = super.startDate;
-		this.endDate = super.endDate;
-		
+		this.invitees = invitees;		
 	}
 
 }
