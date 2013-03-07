2952c5bbf96052476a5a3d7bf8f3be7270f8cef4
Emil Gronnbeck <emilgroenn@gmail.com>
2013-03-07 14:56:02 +0100

    skrevet noe i calendarsim

--- src/cim/models/CalendarSIM.java
+++ src/cim/models/CalendarSIM.java
@@ -1,5 +1,17 @@
 package cim.models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class CalendarSIM {
+	ArrayList<Room> vacantRooms;
+	CalendarObject calendarObject;
+	
+	public CalendarSIM(){
+	}
+	
+	
+	
+	
 
 }
