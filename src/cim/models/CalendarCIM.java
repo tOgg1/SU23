49d46b59afcdc3c8bbf10d13f34ad2e89f5f3caa
Emil Gronnbeck <emilgroenn@gmail.com>
2013-03-08 13:09:06 +0100

    laget dbhandler

---
+++ src/cim/models/CalendarCIM.java
@@ -0,0 +1,17 @@
+package cim.models;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CalendarCIM {
+	ArrayList<Room> vacantRooms;
+	CalendarObject calendarObject;
+	
+	public CalendarCIM(){
+	}
+	
+	
+	
+	
+
+}
