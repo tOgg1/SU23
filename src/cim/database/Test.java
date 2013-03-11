42f35598085e682b26fef1602ad03acecb187bf7
Emil Gronnbeck <emilgroenn@gmail.com>
2013-03-11 12:39:57 +0100

    laget test

---
+++ src/cim/database/Test.java
@@ -0,0 +1,9 @@
+package cim.database;
+
+public class Test {
+	public static void main(String[] args) {
+		DatabaseHandler db = new DatabaseHandler();
+		
+		System.out.println(db.getCalendar(54));
+	}
+}
