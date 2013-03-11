c908fe1a987bd4a475b808935081ce01807572d3
Hakon Amdal <hakon.amdal2@gmail.com>
2013-03-11 11:00:28 +0100

    laget skall pa API til serveren

--- src/cim/net/Request.java
+++ src/cim/net/Request.java
@@ -4,6 +4,15 @@ import java.io.Serializable;
 
 public class Request implements Serializable {
 	private Bucket bucket;
-	private String request; // tab separated request string
+	private String request; // tab separated request string4
+	private Object[] args;
+	
+	public String getRequest() {
+		return this.request;
+	}
+	
+	public Object[] getArgs() {
+		return this.args;
+	}
 	
 }
