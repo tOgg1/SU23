36cd330d68788ea9f175bf667ada844327f88553
Emil Gronnbeck <emilgroenn@gmail.com>
2013-03-11 12:55:14 +0100

    n

--- src/cim/database/DatabaseHandler.java
+++ src/cim/database/DatabaseHandler.java
@@ -77,7 +77,7 @@ public class DatabaseHandler {
 		ResultSet rs = executeQuery(sql);
 		try {
 			rs.next();
-			System.out.println(rs.getString("first_name"));
+			System.out.println(rs.getString("email"));
 			return new Account(rs.getString("first_name"),
 							   rs.getString("last_name"),
 							   rs.getString("email"));
