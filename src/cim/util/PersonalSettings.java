c1a56c62e7f36c053cce71da074d1cfe2da04a49
Hakon Amdal <hakon.amdal2@gmail.com>
2013-03-12 10:01:02 +0100

    test

--- src/cim/util/PersonalSettings.java
+++
@@ -1,7 +0,0 @@
-package cim.util;
-
-public class PersonalSettings {
-	public static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/cim";
-	public static final String MYSQL_USER = "hawk";
-	public static final String MYSQL_PW  = "hawk12345";
-}
