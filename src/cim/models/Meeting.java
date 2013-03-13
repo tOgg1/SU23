4dd729422246dbf146851f34534b8c0c6ab9d316
Emil Gronnbeck <emilgroenn@gmail.com>
2013-03-13 13:55:42 +0100

    jobbet i dbhandler

--- src/cim/models/Meeting.java
+++ src/cim/models/Meeting.java
@@ -2,17 +2,17 @@ package cim.models;
 
 import java.sql.Time;
 import java.sql.Date;
-import java.util.Map;
+import java.util.ArrayList;
 
 public class Meeting extends Appointment
 {
-	private Map<Integer, Attendable> invitees;
+	private ArrayList invitees;
 
     private final static int STATUS_INVITED = 0;
     private final static int STATUS_ACCEPTED = 1;
     private final static int STATUS_DECLINED = 2;
 
-	public Meeting(String info, Map<Integer, Attendable> invitees, Room room, Time startDate, Time endDate, Date date)
+	public Meeting(String info, ArrayList<MeetingResponse> response, Room room, Time startDate, Time endDate, Date date)
 	{	
 		super(startDate, endDate, info, date);
 		this.invitees = invitees;		
