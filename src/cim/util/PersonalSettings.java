8c4fa30171f6dfb6335031ebb84ed59ffd9ca02a
Emil Gronnbeck <emilgroenn@gmail.com>
2013-03-13 14:47:12 +0100

    petter fiksa

--- src/cim/util/PersonalSettings.java
+++ src/cim/util/PersonalSettings.java
@@ -1,14 +1,8 @@
 package cim.util;
 
 public class PersonalSettings {
-<<<<<<< HEAD
-	public static final String JDBC_URL = "jdbc:mysql://78.91.2.123:3306/cim";
-	public static final String MYSQL_USER = "Petter";
-	public static final String MYSQL_PW  = "123456";
-=======
 	public static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/cim";
 	public static final String MYSQL_USER = "cim";
 	public static final String MYSQL_PW  = "cim";
 	public static final String SERVER_IP = "localhost";
->>>>>>> 17470754a624e68687e923224407ad2f7e807518
 }
