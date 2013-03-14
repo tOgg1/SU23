23594d87031dd31e7f09542ad9addb35904f5ebd
Beate <beatehay@stud.ntnu.no>
2013-03-14 15:51:27 +0100

    Struktur i addAppointmentDialog

--- src/cim/views/AddAppointmentDialog.java
+++ src/cim/views/AddAppointmentDialog.java
@@ -6,6 +6,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 import java.awt.Component;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
 
 public class AddAppointmentDialog extends JFrame{
 	private AddAppointmentDetailsPanel addDetailsPanel;
@@ -15,23 +17,35 @@ public class AddAppointmentDialog extends JFrame{
 	
 	public AddAppointmentDialog(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 648, 571);
-		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		setBounds(100, 100, 800, 571);
+//		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		
 		mainPanel = new JPanel();
 		mainPanel.setAlignmentY(Component.TOP_ALIGNMENT);
 		mainPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		getContentPane().add(mainPanel);
-		mainPanel.setLayout(new GridLayout(6, 4, 0, 0));
+		mainPanel.setLayout(new GridLayout(0, 1, 0, 0));
 		
 		addDetailsPanel = new AddAppointmentDetailsPanel();
 		addDetailsPanel.setSize(470, 240);
 		mainPanel.add(addDetailsPanel);
 		
+		JCheckBox chckbxAddParticipants = new JCheckBox("Legg til personer/grupper");
+		mainPanel.add(chckbxAddParticipants);
+		
 		addParticipantsPanel = new AddParticipantsPanel();
 		mainPanel.add(addParticipantsPanel);
 		
+		JCheckBox chckbxLeggTilPersonlig = new JCheckBox("Legg til personlig alarm");
+		mainPanel.add(chckbxLeggTilPersonlig);
+		
 		addAlarmPanel = new AddAlarmPanel();
 		mainPanel.add(addAlarmPanel);
+		
+		JButton btnCancel = new JButton("Avbryt");
+		mainPanel.add(btnCancel);
+		
+		JButton btnSave = new JButton("Legg til i kalender");
+		mainPanel.add(btnSave);
 	}
 }
