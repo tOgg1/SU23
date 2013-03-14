0e3348c535d05ffaafa6405a520c47aa85c0d843
Beate <beatehay@stud.ntnu.no>
2013-03-14 16:58:06 +0100

    slettet klassen appointmentview. Funksjonalitet er flytett til andre klasser

--- src/cim/views/AddAlarmPanel.java
+++ src/cim/views/AddAlarmPanel.java
@@ -3,5 +3,6 @@ package cim.views;
 import javax.swing.JPanel;
 
 public class AddAlarmPanel extends JPanel{
-
+// Felter for å lege til alarm. Bruk absolute layout
+// Panelet er allerede "montert" i AddAppointmentDialog
 }
