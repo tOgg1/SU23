d754d42c21b4879853b07f942e0c24c94aaa07a7
Beate <beatehay@stud.ntnu.no>
2013-03-14 17:54:18 +0100

    Lagt til tomme klasser for de fleste deler av GUI

---
+++ src/cim/views/IncommingAppointmentsView.java
@@ -0,0 +1,7 @@
+package cim.views;
+
+import javax.swing.JPanel;
+
+public class IncommingAppointmentsView extends JPanel{
+// For listing av møter til godkjenning. Panelet legges så i ApplicationWindow som en tab
+}
