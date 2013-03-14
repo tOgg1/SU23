4a34915096ba58a86a0fe49edf71e3c6acfc344e
Beate <beatehay@stud.ntnu.no>
2013-03-14 18:44:34 +0100

    IncommingAppointments paabegynt

--- src/cim/views/IncommingAppointmentsView.java
+++ src/cim/views/IncommingAppointmentsView.java
@@ -1,7 +1,24 @@
 package cim.views;
 
 import javax.swing.JPanel;
+import java.awt.GridLayout;
 
 public class IncommingAppointmentsView extends JPanel{
-// For listing av møter til godkjenning. Panelet legges så i ApplicationWindow som en tab
+	
+	private int appointments;
+	
+	public IncommingAppointmentsView() {
+		appointments = getIncommingAppointments();
+		setLayout(new GridLayout(appointments, 0, 0, 0));
+		
+
+	}
+	
+	private int getIncommingAppointments(){
+	
+		//TODO: Hente ut antall avtaler som ligger på vent.
+		// Dette tallet vil brukes til å lage griddet stort nok.
+	
+		return 10;
+	}
 }
