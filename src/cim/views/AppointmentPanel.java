c14a3c6b9a1dd05b85e88cc0ef4259a0fbb744ab
julie <juliejk92@gmail.com>
2013-03-18 15:32:36 +0100

    AppointmentPanel

--- src/cim/views/AppointmentPanel.java
+++ src/cim/views/AppointmentPanel.java
@@ -3,12 +3,14 @@ package cim.views;
 import cim.models.Appointment;
 import cim.net.Client;
 import cim.util.CloakedIronManException;
+import cim.util.Fonts;
 
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.awt.Font;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,18 +23,102 @@ public class AppointmentPanel extends JPanel implements Comparable
 {
     private Appointment base;
     private PropertyChangeSupport pcs;
+    private JTextField txtTime;
+    private JTextField txtPlace;
+    private JTextField txtOkNum;
+    private JTextField txtDeclinedNum;
+    private JTextField txtGruopNum;
+    public JLabel lblOK;
+    public JLabel lblArrow;
+    public JLabel lblGroup;
+    public JLabel lblDeclined;
+    
     public AppointmentPanel(Appointment base)
     {
         this.base = base;
-        JTextField text = new JTextField(base.getName());
-        text.setEditable(false);
-        this.add(text);
+        setLayout(null);
+        JTextField txtName = new JTextField("Navn");
+        txtName.setBounds(10, 39, 61, 20);
+        txtName.setEditable(false);
+        this.add(txtName);
         
-        JTextPane textPane = new JTextPane();
-        textPane.addMouseListener(new deleteListener());
-        textPane.setText("x");
-        add(textPane);
+        JLabel lblDelete = new JLabel();
+        lblDelete.setBounds(134, 11, 18, 20);
+        lblDelete.setFont(new Font("FontAwesome", Font.PLAIN, 14));
+        lblDelete.addMouseListener(new deleteListener());
+        lblDelete.setText(Fonts.AwesomeIcons.ICON_REMOVE.toString());
+        add(lblDelete);
         pcs = new PropertyChangeSupport(this);
+        
+        JLabel lblEdit = new JLabel("New label");
+        lblEdit.setFont(new Font("FontAwesome", Font.PLAIN, 14));
+        lblEdit.setBounds(106, 14, 18, 14);
+        lblEdit.setText(Fonts.AwesomeIcons.ICON_PENCIL.toString());
+        add(lblEdit);
+        
+        txtTime = new JTextField();
+        txtTime.setText("Tid");
+        txtTime.setEditable(false);
+        txtTime.setBounds(10, 12, 86, 20);
+        add(txtTime);
+        txtTime.setColumns(10);
+        
+        lblArrow = new JLabel("New label");
+        lblArrow.setFont(new Font("FontAwesome", Font.PLAIN, 14));
+        lblArrow.setBounds(134, 41, 11, 14);
+        lblArrow.setText(Fonts.AwesomeIcons.ICON_CARET_DOWN.toString());
+        lblArrow.addMouseListener(new showInfoListener());
+        add(lblArrow);
+        
+        txtPlace = new JTextField();
+        txtPlace.setText("Sted");
+        txtPlace.setVisible(false);
+        txtPlace.setEditable(false);
+        txtPlace.setBounds(10, 70, 86, 20);
+        add(txtPlace);
+        txtPlace.setColumns(10);
+        
+        lblGroup = new JLabel("New label");
+        lblGroup.setVisible(false);
+        lblGroup.setFont(new Font("FontAwesome", Font.PLAIN, 11));
+        lblGroup.setBounds(91, 101, 18, 14);
+        lblGroup.setText(Fonts.AwesomeIcons.ICON_GROUP.toString());
+        add(lblGroup);
+        
+        lblOK = new JLabel("New label");
+        lblOK.setVisible(false);
+        lblOK.setFont(new Font("FontAwesome", Font.PLAIN, 11));
+        lblOK.setBounds(10, 101, 18, 14);
+        lblOK.setText(Fonts.AwesomeIcons.ICON_OK.toString());
+        add(lblOK);
+        
+        lblDeclined = new JLabel("New label");
+        lblDeclined.setVisible(false);
+        lblDeclined.setFont(new Font("FontAwesome", Font.PLAIN, 11));
+        lblDeclined.setBounds(53, 101, 18, 14);
+        lblDeclined.setText(Fonts.AwesomeIcons.ICON_REMOVE.toString());
+        add(lblDeclined);
+        
+        txtOkNum = new JTextField();
+        txtOkNum.setVisible(false);
+        txtOkNum.setEditable(false);
+        txtOkNum.setBounds(28, 98, 15, 20);
+        add(txtOkNum);
+        txtOkNum.setColumns(2);
+        
+        txtDeclinedNum = new JTextField();
+        txtDeclinedNum.setVisible(false);
+        txtDeclinedNum.setEditable(false);
+        txtDeclinedNum.setBounds(66, 98, 15, 20);
+        add(txtDeclinedNum);
+        txtDeclinedNum.setColumns(2);
+        
+        txtGruopNum = new JTextField();
+        txtGruopNum.setVisible(false);
+        txtGruopNum.setEditable(false);
+        txtGruopNum.setColumns(2);
+        txtGruopNum.setBounds(106, 98, 15, 20);
+        add(txtGruopNum);
     }
 
     public Appointment getBase() {
@@ -65,6 +151,20 @@ public class AppointmentPanel extends JPanel implements Comparable
 				pcs.firePropertyChange("delbase", base, null);
 				base = null;}
     	}
+
+    public class showInfoListener extends MouseAdapter{
+    	public void mouseReleased(MouseEvent e){
+            lblArrow.setText(Fonts.AwesomeIcons.ICON_CARET_UP.toString());
+            lblGroup.setVisible(true);
+            txtTime.setVisible(true);
+            txtPlace.setVisible(true);
+            txtOkNum.setVisible(true);
+            txtDeclinedNum.setVisible(true);
+            txtGruopNum.setVisible(true);
+            lblOK.setVisible(true);
+            lblDeclined.setVisible(true);
+    		}
+		}
 	}
     
     
