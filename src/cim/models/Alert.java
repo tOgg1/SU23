0fabd6a2bc67a2f81e16bfb37afcb86b1fe97c0e
Beate <beatehay@stud.ntnu.no>
2013-03-19 19:36:10 +0100

    endret toString i Alert

--- src/cim/models/Alert.java
+++ src/cim/models/Alert.java
@@ -70,7 +70,11 @@ public class Alert extends CalendarObject {
     }
 	
 	public String toString() {
-		return "Alarm for '" + this.owner + "' til '" +  this.appointment + "'";
+//		return "Alarm for '" + this.owner + "' til '" +  this.appointment + "'";
+		return
+			"Husk " + this.appointment.getName()
+			+ " kl. " + this.appointment.getStartFormatted()
+			+ " " + this.appointment.getDateFormatted();
 	}
 	
 	
