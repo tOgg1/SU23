b2450b8e00d502f7f336b1024eb52ef1de322cce
Beate <beatehay@stud.ntnu.no>
2013-03-19 19:33:45 +0100

    Lagt inn metode i Alert

--- src/cim/models/Alert.java
+++ src/cim/models/Alert.java
@@ -48,6 +48,9 @@ public class Alert extends CalendarObject {
 	public boolean isSeen() {
 		return this.isSeen;
 	}
+	public void changeIsSeen(boolean b){
+		this.isSeen = b;
+	}
 	
 	public boolean equals(Object obj) {
         if (obj == null)
